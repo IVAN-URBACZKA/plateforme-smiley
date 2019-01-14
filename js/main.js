b459040e6c5c67d620db389b5f9fae095bc18156
Urbaczka ivan <ivanneurbaczka@gmail.com>
2019-01-14 16:55:28 +0100

    test dom

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
- document.getElementById("test").innerHTML = "Hello World!";
- document.getElementById
+let buttongreen = document.querySelector("#green");
+let buttonyellow = document.querySelector("#yellow");
+
+
+let x = 0;
+let i = 0;
+
+buttongreen.addEventListener("mousemove",function(){
+
+    buttongreen.style.transform = 'rotate('+x+'deg)';  
+
+        x += 10;
+       
+    
+})
+
+buttonyellow.addEventListener("click",function(){
+
+    const arr = ["aqua","pink","purple"];
+       buttonyellow.style.background = arr[i];  
+        i++;
+        if(i === arr.length){
+            i=0;
+        }
+    })
+
+const arr = ["indianred","aqua","pink"];
